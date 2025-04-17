93294b3adc17989f633c5188c5484c76630ba433
ZhilPro27 <zhilalkrisna@gmail.com>
2025-04-17 17:12:40 +0700

    Membuat class player

---
+++ Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUKAN_Budi_Daya_Ikan_
+{
+    class Player
+    {
+        private string name;
+        private int money;
+        private int fishCount;
+        private int points;
+    }
+}
