fa30bc5ef2c78096202a2599324a369c8208ebdb
ZhilPro27 <zhilalkrisna@gmail.com>
2025-04-17 17:15:01 +0700

    Membuat class food

---
+++ Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUKAN_Budi_Daya_Ikan_
+{
+    class Food
+    {
+        private string name;
+        private int price;
+    }
+}
