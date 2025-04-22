2f65536d41cada26861100fe048356287e5d558d
ZhilPro27 <zhilalkrisna@gmail.com>
2025-04-23 05:06:23 +0700

    Membuat prosedur loadData

--- Profile.cs
+++ Profile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,24 @@ namespace BUKAN_Budi_Daya_Ikan_
 {
     public partial class Profile: Form
     {
+        private string connectionString = "Data Source=DESKTOP-FP1P0A6\\ZHILALKRISNA;Initial Catalog=BUKAN_db;Integrated Security=True";
+
         public Profile()
         {
             InitializeComponent();
+        }
+
+        private void loadData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Profile";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dgv_Profile.DataSource = dataTable;
+            }
         }
     }
 }
